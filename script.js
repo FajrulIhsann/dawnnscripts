dfa4513481c360c66826576c1782633965f8b9a9
FajrulIhsann <dawnnbsns@gmail.com>
2024-06-27 12:55:41 +0700

    Menambah notifikasi bila text telah dicopy

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let redzsc = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()'
-let gtgsc = 'loadstring(game:HttpGet("https://isnahamzahpastebin.tech/gantenghubv1_new"))()'
+let gantengsc = 'loadstring(game:HttpGet("https://isnahamzahpastebin.tech/gantenghubv1_new"))()'
 
 function modalparam(script,scriptTitle){
     let judulScript = document.getElementById('exampleModalLabel')
@@ -8,13 +8,23 @@ function modalparam(script,scriptTitle){
     judulScript.textContent = (scriptTitle)
 }
 
+// showCopyNotification
+
+function showSnackBar() {
+    var sb = document.getElementById("snackbar");
+  
+    //this is where the class name will be added & removed to activate the css
+    sb.className = "show";
+  
+    setTimeout(()=>{ sb.className = sb.className.replace("show", ""); }, 2000);
+  }
 
 // Copy function start
 function Copy() {
     let valueIsiScript = document.getElementById('REDzHUB').textContent
 // Copy the text inside the text field
 navigator.clipboard.writeText(valueIsiScript);
-console.log(valueIsiScript)
+showSnackBar()
 }
 
 // Copy function end
