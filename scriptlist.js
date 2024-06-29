be6d17064a79290ec54d2dd97443e112ecb4888f
FajrulIhsann <dawnnbsns@gmail.com>
2024-06-29 16:32:08 +0700

    menambahkan fitur pada icon script

--- scriptlist.js
+++ scriptlist.js
@@ -1,3 +1,4 @@
  let redzScript = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()'
- let gantengScript = 'loadstring(game:HttpGet("https://isnahamzahpastebin.tech/gantenghubv1_new'
+ let gantengScript = 'loadstring(game:HttpGet("https://isnahamzahpastebin.tech/gantenghubv1_new))()'
+ let vectorScript = '_G.Mode = "Eng" loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/VectorHub/main/Loader.lua"))()'
 
