dfe8585db5cf0c85571613d7df97c1a6ff1ac43d
FajrulIhsann <dawnnbsns@gmail.com>
2024-11-04 22:32:52 +0700

    Mengubah beberapa variabel menjadi const

--- js/scriptlist.js
+++ js/scriptlist.js
@@ -1,4 +1,4 @@
- let redzScript = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()'
- let gantengScript = 'loadstring(game:HttpGet("https://isnahamzahpastebin.tech/gantenghubv1_new))()'
- let vectorScript = '_G.Mode = "Eng" loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/VectorHub/main/Loader.lua"))()'
+ const redzScript = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()'
+ const gantengScript = 'loadstring(game:HttpGet("https://isnahamzahpastebin.tech/gantenghubv1_new))()'
+ const vectorScript = '_G.Mode = "Eng" loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/VectorHub/main/Loader.lua"))()'
 
